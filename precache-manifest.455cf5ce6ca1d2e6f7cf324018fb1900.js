95217e55b11fadfc5e3056e7e3ba88c2bb3874ca
Dan Hough <daniel.hough@gmail.com>
2020-11-27 22:29:56 -0800

    Updates

---
+++ precache-manifest.455cf5ce6ca1d2e6f7cf324018fb1900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1b301401b8ae75e10f6",
+    "url": "/static/css/main.3347c1c8.chunk.css"
+  },
+  {
+    "revision": "e1b301401b8ae75e10f6",
+    "url": "/static/js/main.e1b30140.chunk.js"
+  },
+  {
+    "revision": "ae2ee3e66d1c4dca9e83",
+    "url": "/static/js/1.ae2ee3e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae529f992e322693495a0e512fa4f349",
+    "url": "/static/media/skull.ae529f99.svg"
+  },
+  {
+    "revision": "c333dc916cb4621d035acdb8414230e9",
+    "url": "/index.html"
+  }
+];
