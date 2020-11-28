a390b3871fd259d6f60105091d82785967a3b2fb
Dan Hough <daniel.hough@gmail.com>
2020-11-27 22:07:55 -0800

    Added Gosper's Glider Gun

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import GameOfLife from './GameOfLife';
 import * as serviceWorker from './serviceWorker';
 
-const gliderPopulation = [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2],]
+// const gliderPopulation = [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2],]
+const gliderPopulation = [[5, 1], [5, 2], [6, 1], [6, 2], [5, 11], [6, 11], [7, 11], [4, 12], [3, 13], [3, 14], [8, 12], [9, 13], [9, 14], [6, 15], [4, 16], [5, 17], [6, 17], [7, 17], [6, 18], [8, 16], [3, 21], [4, 21], [5, 21], [3, 22], [4, 22], [5, 22], [2, 23], [6, 23], [1, 25], [2, 25], [6, 25], [7, 25], [3, 35], [4, 35], [3, 36], [4, 36],]
 ReactDOM.render(<GameOfLife
-    width={20}
-    height={20}
+    width={40}
+    height={40}
     startingPopulation={gliderPopulation}
     start={true}
     border='marquee'
